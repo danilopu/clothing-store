00f4d17980d08178956f8939799c629c8fcfe5b7
Danilo <danilopuskarevic@gmail.com>
2024-09-25 20:18:48 +0200

    New resources and dataLoader update

--- client/src/utils/dataLoader.js
+++ client/src/utils/dataLoader.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const loadProductData = async () => {
   try {
-    const response = await axios.get('/data.json');
+    const response = await axios.get('/data_compressed.json');
     return response.data.products.map(product => ({
       ...product,
       images: product.images.map(image => `/images/${image}`)
